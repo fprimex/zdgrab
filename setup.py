1568a0990e6ec9e2ca5f01e9d8cfe0de86283900
Brent Woodruff <brent@fprimex.com>
2015-05-05 15:27:59 -0400

    bump to 2.0.1, requires zdesk>=2.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="zdgrab",
-      version="2.0.0",
+      version="2.0.1",
       scripts=["bin/zdgrab", "bin/zdsplode"],
       packages=["zdgrab"],
       description="Get attachments from Zendesk tickets.",
